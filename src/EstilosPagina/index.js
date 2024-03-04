9898ad6dce6fa7565c6baedd50f7adf3e3b85f56
Pedro Klemt <157625587+pedroklemtee@users.noreply.github.com>
2024-03-04 13:50:22 -0300

    removendo o props inativo

--- src/EstilosPagina/index.js
+++ src/EstilosPagina/index.js
@@ -1,13 +1,10 @@
 import './EstilosPagina.css'
 
-const EstilosPagina = (props) => {
+const EstilosPagina = () => {
     return (
         <section className="formulario">
             <form>
                 <h2>Iniciar Formulário</h2>
-                <label>
-                    {props.label}
-                </label>
             </form>
         </section>
     );
