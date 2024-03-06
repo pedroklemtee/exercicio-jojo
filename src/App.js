6a2c626b7c81764c924116921e9681dcfff39351
pedroklemtee <pedro.klemt@outlook.com>
2024-03-06 14:05:51 -0300

    Reparando erros!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ButtonMain from './ButtonMain';
 function App() {
   return (
     <div className="App">
-      <ButtonMain />
+      <ButtonMain/>
     </div>
   );
 }
---
+++ src/Formulario/index.js
@@ -0,0 +1,28 @@
+
+
+const Formulario = () => {
+
+    const perguntas = [
+        {
+            pergunta: "Escolha um prato favorito:",
+            opcoes: ["Pizza", "Hamburguer", "Sushi", "Strogonoff"]
+        },
+        {
+            pergunta: "Qual sua atividade de lazer preferida:",
+            opcoes: ["Ler", "Praticar esportes", "Jogar videogame", "Meditar"]
+        },
+        {
+            pergunta: "Escolha um superpoder:",
+            opcoes: ["Voar", "Invisibilidade", "Superforça", "Ler mentes"]
+        }
+    ];
+
+    return(
+        <div>
+            asdkads
+        </div>
+    )
+
+}
+
+export default Formulario
